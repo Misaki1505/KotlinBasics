587ce00bcf28b5b04be53bea698ce5e2aeac6f1f
Misaki1505 <sawako1505@gmail.com>
2022-07-29 20:13:14 -0500

    Nuevas adiciones

--- app/src/main/java/com/example/helloworldkotlin/HelloWorld.kt
+++ app/src/main/java/com/example/helloworldkotlin/HelloWorld.kt
@@ -1,7 +1,8 @@
 package com.example.helloworldkotlin
 
 fun main(args: Array<String>) {
-
+//I'm learning how to do an app
     println("Welcome Again to Kotlin, Pao!")
+    println("I'm happy to learn")
 
 }
