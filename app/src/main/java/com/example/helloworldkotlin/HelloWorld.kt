644909bb5447139b0358bd4c17b70b9050808a6b
Misaki1505 <sawako1505@gmail.com>
2022-08-04 19:28:02 -0500

    Cambio04082022

--- app/src/main/java/com/example/helloworldkotlin/HelloWorld.kt
+++ app/src/main/java/com/example/helloworldkotlin/HelloWorld.kt
@@ -3,6 +3,19 @@ package com.example.helloworldkotlin
 fun main(args: Array<String>) {
 //I'm learning how to do an app in Kotlin
     println("Welcome Again to Kotlin, Pao!")
-    println("I'm happy to learn")
 
+     //var is variable
+    var Knowledge = "Learn"
+    println(Knowledge)
+
+    // val is inmutable, you can't change the value in other line
+    val catName = "My cat's name is \"Fluffy\""
+    println(catName)
+
+    var line1 = "(\\ (\\"
+    var line2 = "(-.-)"
+    var line3 = "o_(\")(\")"
+    println(line1)
+    println(line2)
+    println(line3)
 }
