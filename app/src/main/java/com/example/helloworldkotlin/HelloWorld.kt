97c3b680e5c80a9dc013b3f7a69a89f2c9933ad9
Misaki1505 <sawako1505@gmail.com>
2022-08-08 19:12:45 -0500

    Cambio08082022

--- app/src/main/java/com/example/helloworldkotlin/HelloWorld.kt
+++ app/src/main/java/com/example/helloworldkotlin/HelloWorld.kt
@@ -18,4 +18,30 @@ fun main(args: Array<String>) {
     println(line1)
     println(line2)
     println(line3)
+
+    val myPet = "Nicolas"
+    val myName = "paula"
+    val myAnimal = "Shark"
+
+    println(myPet.length)
+    println(myName.capitalize())
+    println(myAnimal.decapitalize())
+    println(myPet.get(3))
+    println(myPet[3])
+    println(myPet.substring(3))
+    println(myPet.substring(3,5))
+
+    println(myPet.chunked(2))
+
+    println("My dog's name is $myPet")
+
+    val apples = 3
+    val oranges = 6
+    println("The total number of fruit is ${apples + oranges}")
+
+    val client = "Mary"
+    val products = 3
+    val price = 30
+
+    println("The client $client has to pay ${products * price} dollars")
 }
