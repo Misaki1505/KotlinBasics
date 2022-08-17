7289f219a6bd4b1e9173f3cf095b614d4e807063
Misaki1505 <sawako1505@gmail.com>
2022-08-16 20:08:09 -0500

    Cambio16082022

---
+++ app/src/main/java/com/example/helloworldkotlin/Operators.kt
@@ -0,0 +1,13 @@
+package com.example.helloworldkotlin
+
+fun main(args: Array<String>) {
+    var bikes = 5
+    bikes += 3
+    println(bikes)
+    bikes -= 1
+    println(bikes)
+    bikes *= 2
+    println(bikes)
+    bikes /= 2
+    println(bikes)
+}
