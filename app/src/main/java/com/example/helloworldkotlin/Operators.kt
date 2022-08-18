455d5d77daf262c1efa258c9317a96bcfb4bf647
Misaki1505 <sawako1505@gmail.com>
2022-08-17 20:55:58 -0500

    Cambio17082022

--- app/src/main/java/com/example/helloworldkotlin/Operators.kt
+++ app/src/main/java/com/example/helloworldkotlin/Operators.kt
@@ -10,4 +10,16 @@ fun main(args: Array<String>) {
     println(bikes)
     bikes /= 2
     println(bikes)
+
+    // Logical operators: && = And, || = Or, ! = not
+    // Comparison: > = Greater than, < = Smaller than,
+    // >= = Greater than or equal to, <= = Smaller than or equal to,
+    // == = Equals, != = Not equals
+
+    val Expr1 = 5.374 * 9.5847
+    val Expr2 = 3.237 * 15.3726
+    println("Is $Expr1 > $Expr2 : ${Expr1 > Expr2}")
+
+    val Expr3 = ! ((3>=3) && (false || (2<5)))
+    println(Expr3)
 }
