0bb644c700331f951c868e68cff3c6af8600f7ad
Misaki1505 <sawako1505@gmail.com>
2022-11-04 18:36:33 -0500

    Cambio04112022

--- app/src/main/java/com/example/helloworldkotlin/WhenCondit.kt
+++ app/src/main/java/com/example/helloworldkotlin/WhenCondit.kt
@@ -1,11 +1,22 @@
 package com.example.helloworldkotlin
 
 fun main(args: Array<String>) {
-    val month = "September"
-    val days = when(month) {
-        "January", "March", "May", "July", "August", "October", "december"-> 31
-        "February" -> 28
-        else -> 30
+ //   val month = "September"
+ //   val days = when(month) {
+ //       "January", "March", "May", "July", "August", "October", "december"-> 31
+ //       "February" -> 28
+ //       else -> 30
+ //   }
+ //   println("The month of $month has $days days")
+    val input = readLine()?:"0"
+    var hour = input.toInt()
+    if(hour > 23) hour =23
+    val timeofDay = when(hour) {
+        in 6..11 -> "morning"
+        in 12..14 -> "noon"
+        in 15..17 -> "afternoon"
+        in 18..21 -> "evening"
+        else -> "night"
     }
-    println("The month of $month has $days days")
+    println("At $hour:00 it's $timeofDay")
 }
