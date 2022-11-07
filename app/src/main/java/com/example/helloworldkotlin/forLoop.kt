833fac21b2bdf1c485f4c2e37ba8fc379e6f31ac
Misaki1505 <sawako1505@gmail.com>
2022-11-06 20:18:58 -0500

    Cambio06112022

--- app/src/main/java/com/example/helloworldkotlin/forLoop.kt
+++ app/src/main/java/com/example/helloworldkotlin/forLoop.kt
@@ -6,22 +6,9 @@ fun main(args: Array<String>) {
         println("Feed the $animal")
     }
 
-    for (i in 1..12) {
-        val month = when(i)
-        {
-            1 -> "January"
-            2 -> "February"
-            3 -> "March"
-            4 -> "April"
-            5 -> "May"
-            6 -> "June"
-            7 -> "July"
-            8 -> "August"
-            9 -> "September"
-            10 -> "October"
-            11 -> "November"
-            12 -> "December"
-            else -> " "
-        }
+    var total = 0
+    for(i in 1..100){
+        total += i
     }
+    println("Total is $total")
 }
