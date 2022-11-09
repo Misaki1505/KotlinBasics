8930c49ae1e10da1fb7dea74ca692d64d7b43922
Misaki1505 <sawako1505@gmail.com>
2022-11-08 19:54:01 -0500

    Cambio08112022

---
+++ app/src/main/java/com/example/helloworldkotlin/Matrix.kt
@@ -0,0 +1,22 @@
+package com.example.helloworldkotlin
+
+fun main(args: Array<String>) {
+    for (i in 1..5){
+        for(j in 1..5){
+          print("$i, $j \t")
+        }
+        println()
+    }
+
+    val matrixSize = 10
+    for(i in 0..matrixSize){
+        for (j in 0..matrixSize){
+            if(i==j) {
+                print("diagonal\t")
+            } else {
+                print("$i,$j \t\t")
+            }
+        }
+        println()
+    }
+}
