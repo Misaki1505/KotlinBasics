138e9f9c4524f124e14ba1630b940dc9089fa232
Misaki1505 <sawako1505@gmail.com>
2022-11-12 20:01:30 -0500

    Cambio12112022

---
+++ app/src/main/java/com/example/helloworldkotlin/DoWhileLoop.kt
@@ -0,0 +1,10 @@
+package com.example.helloworldkotlin
+
+fun main(args: Array<String>) {
+    val puzzlePieces = 20
+    var piecesPlaced = 0
+    do {
+        piecesPlaced++
+        println("Placed piece #$piecesPlaced")
+    } while (piecesPlaced < puzzlePieces)
+}
