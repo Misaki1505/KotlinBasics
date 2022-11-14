99d64f54d354415fde3fed4ccd44ff48bc5f05d5
Misaki1505 <sawako1505@gmail.com>
2022-11-13 21:30:03 -0500

    Cambio13112022

--- app/src/main/java/com/example/helloworldkotlin/DoWhileLoop.kt
+++ app/src/main/java/com/example/helloworldkotlin/DoWhileLoop.kt
@@ -7,4 +7,16 @@ fun main(args: Array<String>) {
         piecesPlaced++
         println("Placed piece #$piecesPlaced")
     } while (piecesPlaced < puzzlePieces)
+    var finished = false
+    do {
+        println("please enter a number greater than 100")
+        val input = readLine()?:""
+        var number = input.toInt()
+        if(number > 100) {
+            println("Thanks")
+            finished = true
+        } else {
+            println("$number is not greater than 100. Please try again")
+        }
+    } while (!finished)
 }
